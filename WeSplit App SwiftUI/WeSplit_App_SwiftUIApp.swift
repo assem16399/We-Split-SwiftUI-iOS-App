abf82da09c9ab438288e02b46b9991b872fa401b
assem16399 <aasem.hany@gmail.com>
2024-02-04 09:09:35 +0200

    Initial Commit

---
+++ WeSplit App SwiftUI/WeSplit_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_App_SwiftUIApp.swift
+//  WeSplit App SwiftUI
+//
+//  Created by Aasem Hany on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
