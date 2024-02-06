6d0eaf32fe992a9d187131ed227d3ec0d017b5f2
assem16399 <aasem.hany@gmail.com>
2024-02-06 05:32:57 +0200

    Finsih App

--- WeSplit App SwiftUI/ContentView.swift
+++ WeSplit App SwiftUI/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 10
+    @FocusState private var amountIsFocused: Bool
+    
+    let tips = [5, 10, 20, 0]
+    
+    var totalWithTip: Double {
+        checkAmount + ((checkAmount * Double(tipPercentage)) / 100)
+    }
+    
+    var paymentForEach: Double {
+        let checkAfterTip = totalWithTip
+        return checkAfterTip / Double(numberOfPeople)
+    }
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            
+            Form{
+                
+                Section {
+                    TextField("Check Total Amount",value: $checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "USD" ))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of People",selection: $numberOfPeople){
+                        ForEach(2..<100,id: \.self){
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                
+                Section("Choose Tip Percentage"){
+                    Picker("Tip Percentage",selection: $tipPercentage){
+                        ForEach(0..<101,id: \.self){
+                            Text($0,format: .percent)
+                        }
+                    }
+                }
+                
+                Section("Total With Tip"){
+                    Text(totalWithTip, format: .currency(code: Locale.current.currency?.identifier ?? "USD" ))
+                }
+                
+                Section("Each one pays"){
+                    Text(paymentForEach, format: .currency(code: Locale.current.currency?.identifier ?? "USD" ))
+                }
+                
+            }
+            .toolbar{
+                if amountIsFocused {
+                    Button("Dismiss"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+            .navigationTitle("WeSplit")
+            
         }
-        .padding()
     }
 }
 
